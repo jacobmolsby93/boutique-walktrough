162cf540d413b6a10975a572629f633405104ded
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-05 11:50:39 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, reverse, redirect, HttpResponse
 
 # Create your views here.
 
@@ -7,6 +7,7 @@ def view_bag(request):
     A view to renders the bag content page
     """
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """
@@ -36,3 +37,53 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag  # Updates the bag varible to the updated version.
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """
+    Adjust the quantity of item in the shopping bag
+    """
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST: 
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else: 
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id) 
+    else: 
+        if quantity > 0:
+            bag[item_id] = quantity
+        else: 
+            bag.pop(item_id)
+        
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """
+    Remove the item from the shopping bag
+    """
+    try:
+        size = None
+        if 'product_size' in request.POST: 
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id) 
+        else: 
+            bag.pop(item_id)
+            
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(e, status=500)
