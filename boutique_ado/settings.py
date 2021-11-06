824f47c354c3b51c7c1fe0b7129795c43a171a24
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-06 11:28:04 +0000

    Added basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -167,11 +167,18 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Where all upploaded media files will go
 
-
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')  # Secret key can be used to use anything on stripe. !!!WARNING
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
