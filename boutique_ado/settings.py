348a543ad1ed61b70701d965c4a96edcc315ecb0
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-07 07:27:56 +0000

    Added class methods to webhooks handler and webhook view

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -172,7 +172,8 @@ FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')  # Secret key can be used to use anything on stripe. !!!WARNING
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')  # Secret key can be used for anything on stripe. ! Warning
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
