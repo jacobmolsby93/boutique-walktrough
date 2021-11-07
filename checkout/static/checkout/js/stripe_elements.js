26572e826a69b05c545f48d3599607f5debbf66b
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-07 08:15:12 +0000

    Added checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
-/*
-    Core logic/payment flow for this comes from:
-    https://stripe.com/docs/payments/accept-a-payment
 
-    CSS from here
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
@@ -10,7 +10,6 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
 var style = {
     base: {
         color: '#000',
@@ -19,18 +18,17 @@ var style = {
         fontSize: '16px',
         '::placeholder': {
             color: '#aab7c4'
-        } 
+        }
     },
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
     }
 };
-
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle realtime validation error on the card element
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -39,8 +37,8 @@ card.addEventListener('change', function (event) {
                 <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
-        `
-        $(errorDiv).html(html);        
+        `;
+        $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
@@ -64,17 +62,17 @@ form.addEventListener('submit', function(ev) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
                 <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
             $('#payment-form').fadeToggle(100);
             $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});  // If there is an error, this allows the user to fix it.
-            $('#submit-button').attr('disabled', false);  // and to submit the card again 
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
-                form.submit()
+                form.submit();
             }
         }
     });
