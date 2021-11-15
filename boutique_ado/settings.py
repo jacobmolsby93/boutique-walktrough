a82506b7af9f6063cb64bcd6d621a79b21131875
Jacob Molsby <jacobmolsby@hotmail.se>
2021-11-15 01:34:29 +0000

    Connect all allauth features to the main page site

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -61,7 +61,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGHT = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'), name='allauth'),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls'), name='home'),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
